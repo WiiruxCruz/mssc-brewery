021f2b6875debf4066dc7b190ce3797fc8db281b
WiiruxCruz <hialf.92@gmail.com>
2025-05-27 00:35:40 -0600

    Initial commit project

---
+++ src/test/java/guru/springframework/mssc_brewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.springframework.mssc_brewery;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MsscBreweryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
