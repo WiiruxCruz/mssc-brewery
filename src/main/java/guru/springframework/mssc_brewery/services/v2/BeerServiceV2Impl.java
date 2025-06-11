a34fcc6eece854e60eaad77e09029145371951fb
WiiruxCruz <hialf.92@gmail.com>
2025-06-11 01:07:15 -0600

    Beer API V2

---
+++ src/main/java/guru/springframework/mssc_brewery/services/v2/BeerServiceV2Impl.java
@@ -0,0 +1,33 @@
+package guru.springframework.mssc_brewery.services.v2;
+
+import java.util.UUID;
+
+import guru.springframework.mssc_brewery.web.model.v2.BeerDtoV2;
+
+public class BeerServiceV2Impl implements BeerServiceV2{
+
+	@Override
+	public BeerDtoV2 getBeerById(UUID beerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BeerDtoV2 saveNewBeer(BeerDtoV2 beerDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateBeer(UUID beerId, BeerDtoV2 beerDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(UUID beerId) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- src/main/java/guru/springframework/mssc_brewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/guru/springframework/mssc_brewery/web/controller/v2/BeerControllerV2.java
@@ -1,4 +1,4 @@
-package guru.springframework.mssc_brewery.web.controller;
+package guru.springframework.mssc_brewery.web.controller.v2;
 
 import java.util.UUID;
 
@@ -15,42 +15,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import guru.springframework.mssc_brewery.services.BeerService;
-import guru.springframework.mssc_brewery.web.model.BeerDto;
+import guru.springframework.mssc_brewery.services.v2.BeerServiceV2;
+import guru.springframework.mssc_brewery.web.model.v2.BeerDtoV2;
 
 
-@Deprecated
-@RequestMapping("/api/v1/beer")
+@RequestMapping("/api/v2/beer")
 @RestController
-public class BeerController {
+public class BeerControllerV2 {
 	
-	private final BeerService beerService;
+	private final BeerServiceV2 beerServiceV2;
 	
-	public BeerController(BeerService bs) {
-		this.beerService = bs;
+	public BeerControllerV2(BeerServiceV2 bs) {
+		this.beerServiceV2 = bs;
 	}
 	
 	@GetMapping("/{beerId}")
-	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
+	public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId){
 		
-		return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+		return new ResponseEntity<>(beerServiceV2.getBeerById(beerId), HttpStatus.OK);
 	}
 	
 	@PostMapping //POST - create new beer
-	public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
-		BeerDto savedDto = beerService.saveNewBeer(beerDto);
+	public ResponseEntity handlePost(@RequestBody BeerDtoV2 beerDto) {
+		BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
 		
 		HttpHeaders headers = new HttpHeaders();
 		//todo add hostname to url
-		headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+		headers.add("Location", "/api/v2/beer/" + savedDto.getId().toString());
 		
 		return new ResponseEntity(headers, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/{beerId}")
-	public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+	public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDtoV2 beerDto) {
 		
-		beerService.updateBeer(beerId, beerDto);
+		beerServiceV2.updateBeer(beerId, beerDto);
 		
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
@@ -58,6 +57,6 @@ public class BeerController {
 	@DeleteMapping("/{beerId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteBeer(@PathVariable("beerId") UUID beerId) {
-		beerService.deleteById(beerId);
+		beerServiceV2.deleteById(beerId);
 	}
 }
